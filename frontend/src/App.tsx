1813269ea4aa5a08f1f9fcdec00652f5338ecb91
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-04-28 15:04:35 +0700

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-500">Inventory Management System</h1>
+    </div>
+  )
+}
+
+export default App
