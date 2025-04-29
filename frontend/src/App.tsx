797bf93693893fea51d759485ca9f22d5f277a86
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-04-29 10:30:01 +0700

    protecting dashboard access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );
